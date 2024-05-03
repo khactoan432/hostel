37e59a20ca2873c062d13795afb9858861eab07f
khactoan432 <khactoan432@gmail.com>
2024-05-03 13:56:34 +0700

    fix-cors-2

--- src/services/makeRequest.js
+++ src/services/makeRequest.js
@@ -1,13 +1,16 @@
-import axios from "axios"
+/** @format */
+
+import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'https://bkhostel.hcmut.tech',
+      mode: "cors",
+      baseURL: "https://bkhostel.hcmut.tech",
 });
 
 export async function makeRequest(url, requestOption) {
-    const response = await axiosInstance(url, requestOption)
-        .then(res => res.data)
-        .catch(err => console.log("**Error**: ", err))
-    
-    return response
+      const response = await axiosInstance(url, requestOption)
+            .then((res) => res.data)
+            .catch((err) => console.log("**Error**: ", err));
+
+      return response;
 }
